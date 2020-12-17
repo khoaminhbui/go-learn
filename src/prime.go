f3683de86590da987436c89dd3788f49c67cccf0
Khoa <bmkhoaw@gmail.com>
2020-12-18 00:01:12 +0700

    add test api

--- src/prime.go
+++ src/prime.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
+func getPrime(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(2);
+}
+
 func main() {
-	fmt.Println("hello!")
+	fmt.Println("hello prime!")
+
+	// Init router
+	r := mux.NewRouter()
+
+	// Route handles & endpoints
+	r.HandleFunc("/prime/{n}", getPrime).Methods("GET")
+
+	// Start server
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
