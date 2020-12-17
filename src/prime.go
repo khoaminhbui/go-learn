26bae869f9a595f0d3d105be9a0b27c6c5af74cc
Khoa <bmkhoaw@gmail.com>
2020-12-18 00:40:05 +0700

    calcualte prime using trial division

--- src/prime.go
+++ src/prime.go
@@ -5,13 +5,51 @@ import (
 	"log"
 	"net/http"
 	"encoding/json"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
+type Response struct {
+	ErrorCode	int  	`json:"errorCode"`
+	Message   	string  `json:"message"`
+	Value  		int  	`json:"value"`
+}
+
+func trialDivision(n int) int {
+    for i := n-1; i >= 2; i-- {
+        var isPrime = true;
+        for j := 2; j * j <= i; j++ { 
+            if i % j == 0 {
+                isPrime = false;
+                break; 
+            }
+        } 
+        
+        if isPrime {
+            return i;
+        }
+    }
+
+    return 1;
+}
+
 func getPrime(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	n, err := strconv.Atoi(params["n"])
+
+	response := Response{ErrorCode: 0, Message: "", Value: 0}
+	if err != nil {
+		response.ErrorCode = 1
+		response.Message = "Invalid number"
+	} else {
+		prime := trialDivision(n)
+		response.Message = "Success"
+		response.Value = prime
+	}	
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(2);
+	json.NewEncoder(w).Encode(response);
 }
 
 func main() {
