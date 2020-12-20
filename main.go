a956c7e0c3facf67d66b694fc4e7bff05f639b33
Khoa <bmkhoaw@gmail.com>
2020-12-20 14:46:06 +0700

    remove irrelevant stuff

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/khoaminhbui/go-learn/domain"
 	"github.com/khoaminhbui/go-learn/infra"
 )
 
 func main() {
-	domain.ShowCores()
 	infra.StartSimpleServer()
 }
